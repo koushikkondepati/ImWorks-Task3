c7c2fd7ba2778e88094d4d7f4382fb89fad80d3b
koushikdev1 <koushik@billionlives.in>
2020-11-28 12:26:29 +0530

    dummy tabs added

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,21 @@
   <div>
     <v-app>
       <Navbar />
+      <v-main>
+        <Project />
+      </v-main>
     </v-app>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
+import Project from "./components/ProjectList";
 export default {
   name: "App",
   components: {
-    Navbar
+    Navbar,
+    Project
   }
 };
 </script>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-btn-toggle
+      v-model="toggle_exclusive"
+      rounded
+      class="d-flex justify-center"
+    >
+      <v-btn>
+        Draft
+      </v-btn>
+      <v-btn>
+        Deployed
+      </v-btn>
+      <v-btn>
+        Complted
+      </v-btn>
+      <v-btn>
+        Archived
+      </v-btn>
+    </v-btn-toggle>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tabs",
+  data() {
+    return {
+      toggle_exclusive: undefined
+    };
+  }
+};
+</script>
